6fcd533eba17dbfd02a96902eda3c1024afa032b
Nichol Vacca <nichol_vacca1@baylor.edu>
2022-02-28 11:17:56 -0600

    homework

--- Chapter 10 HW/PatientBill.py
+++ Chapter 10 HW/PatientBill.py
@@ -6,11 +6,11 @@ def main():
 
     patient = pc.Patient(1, "Matt Jones", "123 Main St, Waco, TX 76706", "234-555-7415", "TRUE")
  
-    physical_exam = pr.Procedure("Physical Exam", "2/15/2022", "Dr.Irvine", 250, 1)
+    physical_exam = pr.Procedure("Physical Exam", "2/15/2022", "Dr. Irvine", 250, 1)
 
-    MRI_exam = pr.Procedure("MRI", "2/15/2022", "Dr.Hamilton", 1500, 1)
+    MRI_exam = pr.Procedure("MRI", "2/15/2022", "Dr. Hamilton", 1500, 1)
 
-    CT_scan = pr.Procedure("CT Scan", "2/15/2022", "Dr.Drey", 1200, 2)
+    CT_scan = pr.Procedure("CT Scan", "2/15/2022", "Dr. Drey", 1200, 2)
 
 
     print("\n*** Patient Bill ***")
@@ -22,5 +22,6 @@ def main():
     print()
     print(CT_scan)
     print() 
-    print("Total charge is: " + (physical_exam.charge + MRI_exam.charge + CT_scan.charge))
+    ##print("Total charge is: " + sum(physical_exam.charge, MRI_exam.charge, CT_scan.charge))
+
 main()
