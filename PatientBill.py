e3801fbfebe4a8fc87688743083ecee1fbcf5949
nikkivacca <98358698+nikkivacca@users.noreply.github.com>
2022-02-28 18:31:09 -0600

    Add files via upload

--- PatientBill.py
+++ PatientBill.py
@@ -6,21 +6,34 @@ def main():
 
     patient = pc.Patient(1, "Matt Jones", "123 Main St, Waco, TX 76706", "234-555-7415", "TRUE")
  
-    physical_exam = pr.Procedure("Physical Exam", "2/15/2022", "Dr.Irvine", 250, 1)
+    procedure1 = pr.Procedure("Physical Exam", "2/15/2022", "Dr. Irvine", 250, 1)
 
-    MRI_exam = pr.Procedure("MRI", "2/15/2022", "Dr.Hamilton", 1500, 1)
+    procedure2 = pr.Procedure("MRI", "2/15/2022", "Dr. Hamilton", 1500, 1)
 
-    CT_scan = pr.Procedure("CT Scan", "2/15/2022", "Dr.Drey", 1200, 2)
+    procedure3 = pr.Procedure("CT Scan", "2/15/2022", "Dr. Drey", 1200, 2)
 
 
     print("\n*** Patient Bill ***")
-    print(patient)
-    print()
-    print(physical_exam)
-    print()
-    print(MRI_exam)
-    print()
-    print(CT_scan)
-    print() 
-    print("Total charge is: " + (physical_exam.charge + MRI_exam.charge + CT_scan.charge))
+    print("Name: ", patient.get_name())
+    print("Address: ", patient.get_address())
+    print("Phone: ", patient.get_phone())
+
+    procedures_list = [procedure1,procedure2,procedure3]
+    total_charge = 0 
+    for a in procedures_list:
+        if a.get_patient_ID() == patient.get_ID():
+            print()
+            print("Procedure: ", a.get_proc_name)
+            print("Date: ", a.get_date)
+            print("Practitioner: ", a.get_practitioner)
+            print("Charge: $", format(a.get_charge(), ',.2f'))
+            print()
+            total_charge += a.get_charge()
+
+    if patient.get_veteran_status() == "TRUE":
+        total_charge = .6*total_charge
+        print("Total charge is: $", format(total_charge, ',.2f'))
+    else: 
+        print("Total charge is: $", format(total_charge, ',.2f'))
+
 main()
